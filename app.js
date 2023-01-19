cff63704c3186907ba225c49365e967df10a392a
Dana2025 <horlescu_dana@yahoo.com>
2023-01-19 14:35:05 +0200

    Async Await Version

--- app.js
+++ app.js
@@ -15,64 +15,60 @@ const attack = document.querySelector("#attack");
 const defence = document.querySelector("#defence");
 const speed = document.querySelector("#speed");
 
-function getValue() {
-    let inputValue = document.getElementById('search-input').value;
-    const url = `https://pokeapi.co/api/v2/pokemon/${inputValue}`;
+function getValue() {  
+    getData();
+}
 
-    fetch(url)
-    .then(res => res.json())
-    .then(pokemon => {
-        const urlFirst = pokemon.abilities[0].ability.url;
-        fetch (urlFirst)
-        .then(res => res.json())
-        .then(pokemonA => {
-            let indexA = 0;
-            lenghtA = pokemonA.effect_entries.length;
-            for (let i=0; i<lenghtA; i++) {
-                if (pokemonA.effect_entries[i].language.name == 'en') {
-                    indexA = i;
-                }
-            }
-            descriptionFirst = pokemonA.effect_entries[indexA].effect;
-            if(descriptionFirst.length > 240) descriptionFirst = descriptionFirst.substring(0,240)+"...";
-            firstAbilityDescription.innerHTML = descriptionFirst;
-     
-        })
-        .catch(error => console.log(error))
-        const urlSecond = pokemon.abilities[1].ability.url;
-        fetch (urlSecond)
-        .then(res => res.json())
-        .then(pokemonB => {
-            let indexB = 0;
-            lenghtB = pokemonB.effect_entries.length;
-            for (let i=0; i<lenghtB; i++) {
-                if (pokemonB.effect_entries[i].language.name == 'en') {
-                    indexB = i;
-                }
-            }
-            descriptionSecond = pokemonB.effect_entries[indexB].effect;
-            if(descriptionSecond.length > 240) descriptionSecond = descriptionSecond.substring(0,240)+"...";
-            secondAbilityDescription.innerHTML = descriptionSecond;
-        })
-        .catch(error => console.log(error))
-        let type = pokemon.types[0].type.name;
-        let root =  `images/${type}.jpg`;
-        document.getElementById("card").style.backgroundImage = `url(${root})`;
-        imgIcons.src = `icons/${pokemon.types[0].type.name}.jpg`;
-        imgPokemon.src = pokemon.sprites.other.dream_world.front_default;
-        pokemonName.innerHTML =  pokemon.forms[0].name.toUpperCase();
-        pokemonHp.innerHTML = `HP: ${pokemon.stats[0].base_stat}`;
-        attack.innerHTML = `attack: ${pokemon.stats[1].base_stat}`;
-        defence.innerHTML = `defence: ${pokemon.stats[2].base_stat}`;
-        speed.innerHTML = `speed: ${pokemon.stats[5].base_stat}`;
-        pokemonType.innerHTML = `Type: ${type}`;
-        pokemonHeight.innerHTML = `Height: ${Number(pokemon.height)/10} m`;
-        pokemonWeight.innerHTML = `Weight: ${Number(pokemon.weight)/10} kg`;
-        firstAbilityName.innerText = `1. ${pokemon.abilities[0].ability.name}`;
-        secondAbilityName.innerHTML = `2. ${pokemon.abilities[1].ability.name}`;       
+async function fetchWrapper(url){
+    const response = await fetch(url);
+    return await response.json();
+}
+
+const descriptionAbilities = []
+async function getData(){
+    try{
+        let inputValue = document.getElementById('search-input').value;
+    const url = `https://pokeapi.co/api/v2/pokemon/${inputValue}`;
+    const pokemonData = await fetchWrapper(url);
+    const abilities = [pokemonData.abilities[0].ability.url, pokemonData.abilities[1].ability.url];
+    const promises = [];
+    abilities.forEach(url => {
+        const promise = fetchWrapper(url);
+        promises.push(promise)
+    });
+    const data = await Promise.all(promises)
+    data.forEach(data => {
+        let indexA = 0;
+        lenghtA = data.effect_entries.length;
+        for (let i = 0; i < lenghtA; i++) {
+            if (data.effect_entries[i].language.name == 'en') {
+                indexA = i;
+                let descriptionAbility = data.effect_entries[indexA].effect;
+                if(descriptionAbility.length > 240) descriptionAbility = descriptionAbility.substring(0,240)+"...";
+                descriptionAbilities.push(descriptionAbility)
+            }       
+        }          
     })
-    .catch(error => console.log(error))
+    firstAbilityDescription.innerHTML = descriptionAbilities[0];
+    secondAbilityDescription.innerHTML = descriptionAbilities[1];
+    let type = pokemonData.types[0].type.name;
+    let root =  `images/${type}.jpg`;
+    document.getElementById("card").style.backgroundImage = `url(${root})`;
+    imgIcons.src = `icons/${pokemonData.types[0].type.name}.jpg`;
+    imgPokemon.src = pokemonData.sprites.other.dream_world.front_default;
+    pokemonName.innerHTML =  pokemonData.forms[0].name.toUpperCase();
+    pokemonHp.innerHTML = `HP: ${pokemonData.stats[0].base_stat}`;
+    attack.innerHTML = `attack: ${pokemonData.stats[1].base_stat}`;
+    defence.innerHTML = `defence: ${pokemonData.stats[2].base_stat}`;
+    speed.innerHTML = `speed: ${pokemonData.stats[5].base_stat}`;
+    pokemonType.innerHTML = `Type: ${type}`;
+    pokemonHeight.innerHTML = `Height: ${Number(pokemonData.height)/10} m`;
+    pokemonWeight.innerHTML = `Weight: ${Number(pokemonData.weight)/10} kg`;
+    firstAbilityName.innerText = `1. ${pokemonData.abilities[0].ability.name}`;
+    secondAbilityName.innerHTML = `2. ${pokemonData.abilities[1].ability.name}`;
+    }catch {
+        error => console.log(error);
+    }  
 }
 
 butonSearch.addEventListener('click', getValue);
-
